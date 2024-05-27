4832a6881aedcd3d64703f12742d7d840460e0f8
Sri Kanya Yarramsetti <118448683+SriYarramsetti@users.noreply.github.com>
2024-05-27 17:07:41 +0530

    Create test.py

---
+++ Flask-application/test.py
@@ -0,0 +1,14 @@
+# test_app.py
+
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_home_route(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"Welcome to my Application" in response.data
